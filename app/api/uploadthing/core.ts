7b6ab357691f132cf404e8a977d3c7ff3b0343d1
Umidjon-mobile <umidjoasdasdas@gmail.com>
2025-07-29 00:19:37 +0500

    telegram socket

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,20 +1,28 @@
-import { authOptions } from '@/lib/auth-options'
-import { getServerSession } from 'next-auth'
-import { createUploadthing, type FileRouter } from 'uploadthing/next'
-import { UploadThingError } from 'uploadthing/server'
+import { authOptions } from "@/lib/auth-options";
+import { getServerSession } from "next-auth";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
 
-const f = createUploadthing()
+const f = createUploadthing();
 
 export const ourFileRouter = {
-	imageUploader: f({ image: { maxFileSize: '4MB' } })
-		.middleware(async () => {
-			const token = await getServerSession(authOptions)
-			if (!token) throw new UploadThingError('Unauthorized')
-			return { token }
-		})
-		.onUploadComplete(async ({ file }) => {
-			return file
-		}),
-} satisfies FileRouter
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .middleware(async () => {
+      const token = await getServerSession(authOptions);
+      if (!token) throw new UploadThingError("Unauthorized");
+      return { token };
+    })
+    .onUploadComplete(async ({ file }) => {
+      return {
+        name: file.name,
+        size: file.size,
+        type: file.type,
+        lastModified: file.lastModified,
+        customId: file.customId,
+        key: file.key,
+        url: file.url,
+      };
+    }),
+} satisfies FileRouter;
 
-export type OurFileRouter = typeof ourFileRouter
+export type OurFileRouter = typeof ourFileRouter;
