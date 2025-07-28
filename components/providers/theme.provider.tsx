121e6196d65ceadddfeaa9afd6e6d21515614a29
Umidjon-mobile <umidjoasdasdas@gmail.com>
2025-07-29 00:09:04 +0500

    telegram socket

--- components/providers/theme.provider.tsx
+++ components/providers/theme.provider.tsx
@@ -1,13 +1,15 @@
-'use client'
+"use client";
 
-import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import NoSSR from 'react-no-ssr'
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
-	return (
-		<NextThemesProvider {...props}>
-			<NoSSR>{children}</NoSSR>
-		</NextThemesProvider>
-	)
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return (
+    <NextThemesProvider {...props}>
+      <>{children}</>
+    </NextThemesProvider>
+  );
 }
